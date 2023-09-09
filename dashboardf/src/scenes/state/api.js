f268339595551602d9b9f5ac34e6da0c64cd2ec9
sushilpandeyy <contact.sushilpandey@gmail.com>
2023-09-09 18:15:41 +0530

    New Endpoint for Backend

--- dashboardf/src/scenes/state/api.js
+++ dashboardf/src/scenes/state/api.js
@@ -8,10 +8,14 @@ export const api = createApi({
         getUser: build.query({
             query: (id) => 'general/user/' + id,
             providesTags: ["User"]
-        })
-    })
+        }),
+        getProducts: build.query({
+            query: ()=> "client/products",
+            providesTags: ["Products"],
+        }),
+    }),
 });
 
 export const {
-    useGetUserQuery,
+    useGetUserQuery, useGetProductsQuery
 } = api;

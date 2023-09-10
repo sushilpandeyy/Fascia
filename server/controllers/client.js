e72e7071636953d56135940ad9b36661e0c4c493
sushilpandeyy <contact.sushilpandey@gmail.com>
2023-09-10 11:16:44 +0530

    User was not defined in last backend

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -1,5 +1,6 @@
 import Product from "../models/Product.js"
 import ProductStat from "../models/ProductStat.js"
+import User from "../models/User.js"
 
 export const getProducts = async(req, res) => {
     try {
